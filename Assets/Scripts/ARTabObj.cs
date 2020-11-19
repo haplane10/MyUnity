177a0c7f676f6404a0a89a88e02961d18a8b8eb4
haplane5 <haplane5@gmail.com>
2020-11-19 18:09:46 +0900

    AR Foundation

---
+++ Assets/Scripts/ARTabObj.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class ARTabObj : MonoBehaviour
+{
+    public GameObject prefab;
+
+    private GameObject spawnObj;
+    private ARRaycastManager aRRaycastManager;
+    private Vector2 touchPosition;
+
+    static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    private void Awake()
+    {
+        aRRaycastManager = GetComponent<ARRaycastManager>();
+    }
+    
+    private bool TryGetTouchPosition(out Vector2 touchPosition)
+    {
+        if (Input.touchCount > 0)
+        {
+            touchPosition = Input.GetTouch(0).position;
+            return true;
+        }
+
+        touchPosition = default;
+        return false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!TryGetTouchPosition(out Vector2 touchPosition))
+        {
+            return;
+        }
+
+        if (aRRaycastManager.Raycast(touchPosition, hits, TrackableType.PlaneWithinPolygon))
+        {
+            var hitPose = hits[0].pose;
+
+            if (spawnObj == null)
+            {
+                spawnObj = Instantiate(prefab, hitPose.position, hitPose.rotation);
+            }
+            else
+            {
+                spawnObj.transform.position = hitPose.position;
+            }
+        }
+    }
+}
