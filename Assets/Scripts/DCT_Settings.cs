2d29847cb538d02ce793b7c8f0ee8ef5af5102aa
JungHo Park <haplane5@gmail.com>
2020-12-14 13:53:28 +0900

    Add files via upload

---
+++ Assets/Scripts/DCT_Settings.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DCT;
+
+public class DCT_Settings : Singleton<DCT_Settings>
+{
+    private DCT.PlayMode playMode = DCT.PlayMode.PC;
+    public DCT.PlayMode PlayMode
+    {
+        get
+        {
+            return playMode;
+        }
+    }
+
+    void Awake()
+    {
+        if (UnityEngine.XR.XRSettings.enabled)
+            playMode = DCT.PlayMode.VR;
+        else
+            playMode = DCT.PlayMode.PC;
+    }
+}
